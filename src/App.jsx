c37d4b1e63933d1f9cbcba4499f6e51409e5266f
sai-oduri <saioduri95@gmail.com>
2024-06-20 20:29:05 +0530

    multiple states

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,25 @@
-import { useEffect, useState } from 'react'
-import axios from "axios";
-import './App.css'
-
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [z, setZ] = useState(0);
 
-  const [x, setX] = useState(1);
+  function cal(e) {
+    const inputValue = parseInt(e.target.value) || 0;
+    setX(e.target.value);
+    const val = (inputValue * (inputValue + 1)) / 2;
+    setY(val);
+  }
 
   return (
     <div>
-      <button onClick={() => setX(1)}>1</button>
-      <button onClick={() => setX(2)}>2</button>
-      <button onClick={() => setX(3)}>3</button>
-      <Todo id={x} />
+      <input onChange={cal} type="number" />
+      <p>Sum is {y}</p>
+      <button onClick={() => setZ(z + 1)}>Counter : {z}</button>
     </div>
-  )
+  );
 }
 
-function Todo({ id }) {
-
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    axios.get("https://sum-server.100xdevs.com/todos?id=" + id).then(
-      function (response) {
-        setTodos(response.data.todos);
-      }
-    )
-
-  }, [id]);
-
-  return <div>
-    <p>{id}</p>
-    {
-      todos.map((td) => (
-        <div>
-          <h1>{td.title}</h1>
-          <h1>{td.description}</h1>
-        </div>
-      ))
-    }
-  </div>
-
-}
-
-export default App
+export default App;
